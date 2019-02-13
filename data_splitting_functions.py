6661d24f7bc62b3df600879d7ce23136941e5e2e
ryjo44 <ryan.capps@vanderbilt.edu>
2019-02-13 15:04:17 -0600

    implemented splitting into testing & training data

--- data_splitting_functions.py
+++ data_splitting_functions.py
@@ -4,9 +4,27 @@ import random
 
 # Load in dataset being used
 from dataset import *
-data, label = load_tennis_dataset()
+data, labels = load_custom_dataset()
 
-def split_train_test(data, label):
+def split_train_test(data, labels):
+    # find 10% of total size of dataset
     length = len(data)
     ten_percent = int(length * 0.1)
-    
+
+    # randomly select indices of 10% of set
+    indices = random.sample(range(length), ten_percent)
+
+    test_data = []
+    test_labels = []
+    train_data = data
+    train_labels = labels
+
+    for i in sorted(indices, reverse = True):
+        test_data.append(data[i])
+        test_labels.append(labels[i])
+        del train_data[i]
+        del train_labels[i]
+
+    return train_data, train_labels, test_data, test_labels
+
+split_train_test(data, labels)
