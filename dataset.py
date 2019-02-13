747d7de2b5c32735ce7201c71777b43a0bdae1c2
ryjo44 <ryan.capps@vanderbilt.edu>
2019-02-12 18:03:38 -0600

    consolidated dataset creation

--- dataset.py
+++ dataset.py
@@ -26,4 +26,34 @@ def load_custom_dataset():
     set = []
     labels = []
 
+    # Open file with breast cancer data
+    fileref = open(os.path.join(sys.path[0], "breast-cancer-wisconsin.data"), "r")
+
+    # Iterate through each line of file
+    for line in fileref:
+        line = line.rstrip() # remove EOL chars
+        line_vals = line.split(",") # seperate the features
+
+        # Convert all features in the current line to integers
+        # if an NA value is found (represented as '?' in dataset), exclude from
+        # reature vector
+        for i in range(0, len(line_vals)):
+            if line_vals[i] == '?':
+                break
+            line_vals[i] = int(line_vals[i])
+            set.append(line_vals)
+
+    # Label extraction
+    for i in range(0, len(set)):
+        labels.append(set[i][10])
+
+    # Remove labels from feature vectors
+    tmp = []
+
+    # iterate through all elements in feature array
+    # remove last value (label)
+    for element in set:
+        tmp.append(element[0:10])
+    set = tmp
+
     return set, labels
