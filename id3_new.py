062a348b9c70ad0ccd59731948e54275578fdc48
Sangwon Jeong <swj0418@hotmail.com>
2019-02-16 14:26:37 -0600

    .
    Signed-off-by: Sangwon Jeong <swj0418@hotmail.com>

--- id3_new.py
+++ id3_new.py
@@ -37,22 +37,24 @@ class ID3:
     def build(self):
         self.root = self.run_id3(self.data, self.label, target_attribute=self.target, attributes=[0, 1, 2, 3])
 
-    def predict(self, attribute):
+    def predict(self, tree, attribute):
         print("===================== Prediction ======================")
-        print("Depth 0: ", "parent attribute: ", self.root.parent_attribute, "  my_attribute: ", self.root.attribute,
-              "  my_value: ", self.root.value)
-        
-        for child in self.root.children:
-            print("Depth 1: ", "parent attribute: ", child.parent_attribute, "  my_attribute: ", child.attribute,
-                  "  my_value: ", child.value)
+        first_attribute_true = tree.attribute # Outlook
+        value = attribute[first_attribute_true]
+        print(first_attribute_true)
+        for child in tree.children:
+            edge_value = child.parent_attribute
+            if edge_value is not None:
+                # Directly to the answer. Directly to the leaf node
+                if edge_value == value:
+                    print("Prediction: ", child.value)
+                    return child.value
+            else:
+                # If edge value is None
+                # To the attribute node
+                print("entered")
+                self.predict(child, attribute)
 
-            for c in child.children:
-                print("Depth 2: ", "parent attribute: ", c.parent_attribute, "  my_attribute: ", c.attribute,
-                      "  my_value: ", c.value)
-                for final in c.children:
-                    print("Depth 3: ", "parent attribute: ", final.parent_attribute, "  my_attribute: ",
-                          final.attribute,
-                          "  my_value: ", final.value)
 
     def run_id3(self, examples, labels, target_attribute, attributes, parent_attribute=None):
         """
@@ -63,25 +65,25 @@ class ID3:
         :return:
         """
         root = Node()
-        print(examples, "    ", labels, "    ", target_attribute, "    ", attributes)
+        # print(examples, "    ", labels, "    ", target_attribute, "    ", attributes)
 
         if self.check_all_positive(labels):
             # Return root node with pos/yes label
             root.parent_attribute = parent_attribute
             root.value = 1
-            print("All positive: ", root.value)
+            # print("All positive: ", root.value)
             return root  # , 1 # 1 as in positive label
         elif self.check_all_negative(labels):
             # Return root node with neg/no label
             root.parent_attribute = parent_attribute
             root.value = 0
-            print("All negative: ", root.value)
+            # print("All negative: ", root.value)
             return root  # , 0 # Negative label
         elif len(attributes) == 0:
             # No attributes left
             # Return root node with label the most common value of classification attribute in example
             pred = self.get_most_popular(labels)
-            print("Most common attribute: ", pred)
+            # print("Most common attribute: ", pred)
             root.parent_attribute = parent_attribute
             root.value = pred
             return root  # , self.get_most_popular(example_labels=labels)
@@ -116,7 +118,7 @@ class ID3:
                     v = self.get_most_popular(labels)
                     node.value = v
                 else:
-                    print("Calling iter: ", self.iteration, attributes, "    ", new_attributes, "   ", best_attribute, "   ", retrieve_tennis_attribute_label(best_attribute))
+                    # print("Calling iter: ", self.iteration, attributes, "    ", new_attributes, "   ", best_attribute, "   ", retrieve_tennis_attribute_label(best_attribute))
                     # Below this new branch add a subtree
                     self.iteration += 1
                     node = self.run_id3(branch_example, branch_label, target_attribute, new_attributes, parent_attribute=value)
@@ -198,3 +200,4 @@ if __name__ == '__main__':
     id3.build()
 
     id3.print_tree()
+    id3.predict(id3.root, [0, 1, 1, 1])
